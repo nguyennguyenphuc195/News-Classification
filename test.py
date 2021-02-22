eb5e28b01ab591d3e1879c0315395f048b9b75e4
nguyen nguyen <chrisnguyen9979@gmail.com>
2021-02-22 15:40:25 +0700

    add test.py

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 from text_classfier.dataset import load_dataset
-from text_classfier.preprocess import preprocess_for_train
+from text_classfier.preprocess import preprocess_for_test
 from text_classfier.model import create_model
 import json
 import os
 import pickle
+import numpy as np
 
 with open("ClassifierConfig.json") as f:
     config = json.load(f)
 
 data_dir      = config["DATA_DIR"]
+test_dir      = config["TEST_DATA_DIR"]
 ntoken        = config["N_TOKENS"]
 maxlen        = config["MAXLEN"]
 embedding_dim = config["EMBEDDING_DIM"] 
@@ -26,5 +28,26 @@ if __name__ == "__main__":
     nclass = len(cate2idx)
     model = create_model(ntoken, embedding_dim, nclass)    
     model.load_weights(os.path.join(model_path, "model"))    
+    model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-    model.summary()
+    dataset = load_dataset(test_dir)
+
+    data     = dataset["data"]
+    labels   = dataset["label"]
+
+    x_train = preprocess_for_test(data, word2idx, maxlen)
+    y_train = np.array(labels).reshape((-1, 1))
+
+    output = model.evaluate(x_train, y_train, verbose=0)
+    print("*" * 100)
+    print("Loss:     ", output[0])
+    print("Accuracy: ", output[1])
+    print("*" * 100)
+
+
+
+
+
+
+
+
